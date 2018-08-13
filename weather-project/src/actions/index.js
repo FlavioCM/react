8347d48f06d667502b2962b946f2f07ee6e6de9b
Flavio Medrano <fcmedran@usc.edu>
2018-08-13 11:39:55 -0700

    Update index.js
    erased google API key since it's public directory

--- weather-project/src/actions/index.js
+++ weather-project/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '12eb56349d8d91b6b525df99e75715f4';
+const API_KEY = 'x';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
